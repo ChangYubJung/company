b192e80182ce9352f072a4fa1098aa330e531945
Chang Yub Jung <jwdo3015@naver.com>
2023-03-02 21:11:50 +0900

    20230302 21:11 Select3 까지 공부

--- JS/tab.js
+++ JS/tab.js
@@ -23,8 +23,8 @@
 
 /* childElement 쓸때 class로 하려면 인덱싱 해야 작동.
  id 값은 고유해서 인덱싱 불필요.*/
-const parent = document.getElementsByClassName('list')[0];
-console.log(parent.childElementCount);
+// const parent = document.getElementsByClassName('list')[0];
+// console.log(parent.childElementCount);
 
 
 // jquery로 같은 class명의 갯수를 찾아도됨.
@@ -37,7 +37,7 @@ var count  = $('.tab-button').length;
 // }
 
 // 탭기능 다르게 만들기 -> 이벤트리스너 1개만 쓰기
-$('.list').on('click', function(e){
+$('.tab-button').on('click', function(e){
     // if(e.target == document.querySelectorAll('.tab-button')[0]){
     //     탭열기(0);
     // }
@@ -47,10 +47,101 @@ $('.list').on('click', function(e){
     탭열기(e.target.dataset.id);
 });
 
-
 function 탭열기(구멍){
     $('.tab-button').removeClass('orange');
     $('.tab-content').removeClass('show');
     $('.tab-content').eq(구멍).addClass('show');
     $('.tab-button').eq(구멍).addClass('orange');
 }
+
+// Array / 자료간 순서가 존재. / 자료간 정렬 및 slice 가능.
+var car = ['소나타', 50000, 'white']; 
+// Object Key Value 형태로 데이터 저장. 순서개념이 없음.
+var car2 = {name: '소나타', price: [50000, 3000, 4000],
+ color: 'white'}
+console.log(car2.name);
+
+$('.title').html("상품명" + " " + car2.name);
+$('.price').html("가격" + " " + car2.price[0]);
+
+// 숙제
+var products = [
+    { id: 0, price: 70000, title: 'Blossom Dress' },
+    { id: 1, price: 50000, title: 'Springfield Shirt' },
+    { id: 2, price: 60000, title: 'Black Monastery' }
+];
+
+for(let i = 0 ; i < 3 ; i++){
+    list(i);
+    console.log(i);
+}
+function list(구멍){
+    console.log(구멍);
+    $('.card-body h5').eq(구멍).html(products[구멍].title);
+    $('.card-body p').eq(구멍).html(products[구멍].price);
+}
+
+/*1. 문자 중간에 엔터키 칠 수 있음
+
+2. 중간에 ${ 변수명 } 문법을 이용가능함
+
+${ } 이건 문자안에 변수넣어주는 간단한 문법입니다.
+
+일반 따옴표안에선 사용불가능합니다. */
+
+/*스크립트 안에 코드는 페이지 로드시 1회시 실행됨.
+그래서 이벤트 발생때마다 코드 실행하기 위해서는 이벤트리스너를 활용*/
+
+// select는 input이랑 같은 그래서 input 이벤트 발생.
+
+var pants = [28, 30, 32];
+var shirt = [95, 100 ,105];
+
+$('.form-select').eq(0).on('input', function(){
+    var value = $('.form-select').eq(0).val();
+    if(value == "셔츠"){
+        $('.form-select').eq(1).removeClass('hide');
+        // var 템플릿 = `<option>95</option>
+        // <option>100</option>`
+        // $('.form-select').eq(1).html(템플릿);
+        $('.form-select').eq(1).html('');
+        shirt.forEach(function(data){
+            $('.form-select').eq(1).append(`<option>${data}</option>`);
+        });
+    }else if(value == "바지"){
+        $('.form-select').eq(1).removeClass('hide');
+        $('.form-select').eq(1).html('');
+        // Array 에 붙일 수 있는 forEach 반복문.
+        // 콜백함수 안에 변수 data안에 Array안에 값이 들어가 있음.
+        // forEach안에 파라미터2개 생성가능 첫째는 데이터, 둘째는 0부터 1씩증가하는 정수.
+        pants.forEach(function(data){
+            $('.form-select').eq(1).append(`<option>${data}</option>`);
+        });
+    }
+    else {
+        $('.form-select').eq(1).addClass('hide');
+    }
+});
+var obj = {name: 'kim', age: 20};
+// Object 자료 갯수만큼 반복문 돌리는방법
+for (var key in obj){
+    console.log(key);
+    console.log(obj[key]);
+}
+
+// 자바스크립트로 html 생성법 1
+// var a = document.createElement('p');
+// a.innerHTML = 'hi';
+// document.querySelector('#test').appendChild(a);
+
+// 생성법 2 insertAdjacentHTML, append 둘다 추가해주는 문법임.
+var 템플릿 = '<p>안녕</p>';
+document.querySelector('#test').insertAdjacentHTML('beforeend', 템플릿);
+$('#test').append(템플릿);
+
+/*그냥 함수와 arrow function의 기능차이는 하나가 있는데
+함수 안에서 this를 써야할 경우 
+- 그냥 함수는 함수 안에서 this를 알맞게 재정의해줍니다.
+- arrow function은 함수 안에서 this를 재정의해주지 않고 바깥에 있던 this를 그대로 씁니다.
+그래서 이벤트리스너 콜백함수안에서 this를 써야하면 arrow function 쓰면 의도와 다르게 동작할 수도 있습니다.
+그런데선 쓰지말자. */ 
